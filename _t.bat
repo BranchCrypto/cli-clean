8c306dc171641342754309a446faff42f277f63f
BranchCrypto <BranchCrypto@users.noreply.github.com>
2026-03-19 14:36:50 +0800

    chore: remove tracked temp bat

--- _t.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-set HTTPS_PROXY=http://127.0.0.1:7890
-set HTTP_PROXY=http://127.0.0.1:7890
-
-cd /d C:\Users\Administrator\Desktop\cli-clean
-git add -A
-git commit -m "chore: cleanup and ignore temp bat files"
-git push origin main
