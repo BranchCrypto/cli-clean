57f8b0c67c44e00b7c0b19b84ac2997883810337
BranchCrypto <BranchCrypto@users.noreply.github.com>
2026-03-19 14:35:26 +0800

    chore: remove temp bat files

---
+++ _cleanup.bat
@@ -0,0 +1,8 @@
+@echo off
+set HTTPS_PROXY=http://127.0.0.1:7890
+set HTTP_PROXY=http://127.0.0.1:7890
+
+cd /d C:\Users\Administrator\Desktop\cli-clean
+git add -A
+git commit -m "chore: remove temp bat files"
+git push origin main
