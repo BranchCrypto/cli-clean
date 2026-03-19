199f14f26cddc66cd3b944d0863447334d6d6802
BranchCrypto <BranchCrypto@users.noreply.github.com>
2026-03-19 14:35:55 +0800

    chore: cleanup and ignore temp bat files

--- _t.bat
+++ _t.bat
@@ -4,5 +4,5 @@ set HTTP_PROXY=http://127.0.0.1:7890
 
 cd /d C:\Users\Administrator\Desktop\cli-clean
 git add -A
-git commit -m "chore: cleanup"
+git commit -m "chore: cleanup and ignore temp bat files"
 git push origin main
